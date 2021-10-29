b42a12a032c263fd76d1d2fca409bd978a0e0bdc
Vagyasri <vagyasri2011@gmail.com>
2021-10-29 09:03:04 +0200

    Use filter for 1500s inventors

--- src/index.js
+++ src/index.js
@@ -55,6 +55,8 @@ const people = [
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
+const inventors1500 = inventors.filter((inventor) => inventor.year >= 1500 && inventor.year < 1600);
+
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 
