9d76b543a75999aa4e9c185fd59598a692ddaa05
Vagyasri <vagyasri2011@gmail.com>
2021-10-30 22:44:13 +0200

    Use some method

--- src/index.js
+++ src/index.js
@@ -45,12 +45,29 @@ const inventors = [
   },
 ];
 
-const people = [
+const peopleList = [
   'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
   'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
   'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
   'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
   'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank',
+];
+
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
 // Array.prototype.filter()
@@ -101,8 +118,8 @@ console.table(oldest);
 // console.table(de);
 
 // 7. sort Exercise
-// Sort the people alphabetically by last name
-const alpha = people.sort((lastOne, nextOne) => {
+// Sort the peopleList alphabetically by last name
+const alpha = peopleList.sort((lastOne, nextOne) => {
   const [aLast] = lastOne.split(', ');
   const [bLast] = nextOne.split(', ');
   // const bLast = nextOne.split(', '); // same result
@@ -123,3 +140,18 @@ const instances = data.reduce((obj, item) => {
 },
 {});
 console.table(instances);
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const is19 = people.some((person) => (2021 - person.year) >= 19);
+console.log(is19);
+
+// Array.prototype.every() // is everyone 19 or older?
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
