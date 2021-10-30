5e0d306d2e2a6e25ac4d58f7a588232e9b98e220
Vagyasri <vagyasri2011@gmail.com>
2021-10-30 23:03:19 +0200

    Use find Method

--- src/index.js
+++ src/index.js
@@ -154,6 +154,8 @@ console.log(all19);
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const comment = comments.find((comment) => comment.id === 823423);
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
